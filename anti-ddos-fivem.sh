4fd1d46a49774c448ebd201e6dd63dbfd272e56f
Johan_Paam <54694106+JohanPaam@users.noreply.github.com>
2020-09-19 10:58:30 +0200

    Update anti-ddos-fivem.sh

--- anti-ddos-fivem.sh
+++ anti-ddos-fivem.sh
@@ -6,7 +6,7 @@
 
 # On defini les variables
 #SYSCTL=/usr/bin/sysctl
-IPTABLES=/usr/bin/iptables
+IPTABLES=/sbin/iptables
 IPTABLES_SAVE=/usr/bin/iptables-save
 IPTABLES_RULE_FILE=/etc/iptables/iptables.rules
 IP_ADDR=192.168.1.70
@@ -23,22 +23,22 @@ SSHD=22     # SSHd server port
 #$SYSCTL net.ipv4.tcp_rfc1337="1"
 
 # On supprime toutes les règles deja existantes
-sudo iptables -F
-sudo iptables -X
+$IPTABLES -F
+$IPTABLES -X
 
 # On Drop tout les paquets par défaut 
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -P OUTPUT DROP
+$IPTABLES -P INPUT accept
+$IPTABLES -P FORWARD accept
+$IPTABLES -P OUTPUT accept
 
 # Création de chaines
-sudo iptables -N COMMON
-sudo iptables -N PORTSCAN
-sudo iptables -N SPOOFING
-sudo iptables -N ICMP_IN
-sudo iptables -N UDP_IN
-sudo iptables -N TCP_IN
-sudo iptables -N TCP_OUT
+$IPTABLES -N COMMON
+$IPTABLES -N PORTSCAN
+$IPTABLES -N SPOOFING
+$IPTABLES -N ICMP_IN
+$IPTABLES -N UDP_IN
+$IPTABLES -N TCP_IN
+$IPTABLES -N TCP_OUT
 
 
 
@@ -48,25 +48,25 @@ sudo iptables -N TCP_OUT
 #
 
 # On envoi tout les traffic a la chaine Portscan
-sudo iptables -A INPUT -j PORTSCAN
+$IPTABLES -A INPUT -j PORTSCAN
 
 # On envoi tout le traffic a la chaine Common
-sudo iptables -A INPUT -j COMMON
+$IPTABLES -A INPUT -j COMMON
 
 # On envoi tout le traffic a la chaine Spoofin
-sudo iptables -A INPUT -j SPOOFING
+$IPTABLES -A INPUT -j SPOOFING
 
 # On envoi tout le traffic ICMP a la chaine ICMP
-sudo iptables -A INPUT -p icmp -j ICMP_IN
+$IPTABLES -A INPUT -p icmp -j ICMP_IN
 
 # On envoi tout le traffic UDP a la chaine UDP_IN
-sudo iptables -A INPUT -p udp -j UDP_IN
+$IPTABLES -A INPUT -p udp -j UDP_IN
 
 # On envoi tout le traffic TCP a la chaine TCP_IN
-sudo iptables -A INPUT -p tcp -j TCP_IN
+$IPTABLES -A INPUT -p tcp -j TCP_IN
 
 # On envoi tout les Ttaffic TCP a la chaine TCP_OUT
-sudo iptables -A OUTPUT -p tcp -j TCP_OUT
+$IPTABLES -A OUTPUT -p tcp -j TCP_OUT
 
 
 
@@ -75,23 +75,23 @@ sudo iptables -A OUTPUT -p tcp -j TCP_OUT
 #
 
 # Drop les attaques de type Portscan
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ACK,FIN FIN -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ACK,PSH PSH -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ACK,URG URG -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ALL ALL -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ALL NONE -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
-sudo iptables -A PORTSCAN -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ACK,FIN FIN -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ACK,PSH PSH -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ACK,URG URG -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ALL ALL -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ALL NONE -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+$IPTABLES -A PORTSCAN -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 
 # Return to parent chain
-sudo iptables -A PORTSCAN -j RETURN 
+$IPTABLES -A PORTSCAN -j RETURN 
 
 # On bloque l'ICMP (le Ping)
-sudo iptables -A ICMP_IN -j REJECT --reject-with icmp-proto-unreachable
+$IPTABLES -A ICMP_IN -j REJECT --reject-with icmp-proto-unreachable
 
 
 #
@@ -99,22 +99,22 @@ sudo iptables -A ICMP_IN -j REJECT --reject-with icmp-proto-unreachable
 #
 
 # Drop packets claiming to be the loopback interface (protects against source quench)
-sudo iptables -A SPOOFING ! -i lo -d 127.0.0.0/8 -j DROP
+$IPTABLES -A SPOOFING ! -i lo -d 127.0.0.0/8 -j DROP
 
 # Drop spoofed packets pretending to be from your IP address
-sudo iptables -A SPOOFING ! -i lo -s $IP_ADDR -j DROP
+$IPTABLES -A SPOOFING ! -i lo -s $IP_ADDR -j DROP
 
 # Drop "Class D" multicast addresses. Multicast is illegal as a source address.
-sudo iptables -A SPOOFING -s 224.0.0.0/4 -j DROP
+$IPTABLES -A SPOOFING -s 224.0.0.0/4 -j DROP
 
 # Refuse "Class E" reserved IP addresses
-sudo iptables -A SPOOFING -s 240.0.0.0/5 -j DROP
+$IPTABLES -A SPOOFING -s 240.0.0.0/5 -j DROP
 
 # Drop broadcast address packets
-sudo iptables -A SPOOFING ! -i lo -d $BROADCAST -j DROP
+$IPTABLES -A SPOOFING ! -i lo -d $BROADCAST -j DROP
 
 # Return to parent chain
-sudo iptables -A SPOOFING -j RETURN
+$IPTABLES -A SPOOFING -j RETURN
 
 
 
@@ -123,42 +123,42 @@ sudo iptables -A SPOOFING -j RETURN
            ####
            
 # Limitation des paquets / secondes en UDP sur le port 30120
-sudo iptables -A INPUT -p UDP_IN --destination-port 30120 -m state --state NEW -m limit --limit 8/s --limit-burst 10 -j ACCEPT
-sudo iptables -A INPUT -p TCP_IN --destination-port 30120 -m limit --limit 14/s -j ACCEPT
+$IPTABLES -A INPUT -p UDP_IN --destination-port 30120 -m state --state NEW -m limit --limit 8/s --limit-burst 10 -j ACCEPT
+$IPTABLES -A INPUT -p TCP_IN --destination-port 30120 -m limit --limit 14/s -j ACCEPT
 
 # Limitation des paquets / secondes en TCP sur le port 30120 pour mieux encaisser les attaques de type Layer 7
-sudo iptables -A OUTPUT -p TCP_OUT --destionation-port 30120 -m limit --limit 4/s -j ACCEPT
+$IPTABLES -A OUTPUT -p TCP_OUT --destionation-port 30120 -m limit --limit 8/s -j ACCEPT
 
 # Limitation des paquets / secondes en UDP sur les connexions établies sur le port 30120
-sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -m limit --limit 16/s --limit-burst 20 -j ACCEPT
+$IPTABLES -A INPUT -m state --state RELATED,ESTABLISHED -m limit --limit 16/s --limit-burst 20 -j ACCEPT
 
 
 # Protection Machine
 # On force la vérification des Paquets en SYN
-sudo iptables -A COMMON -p tcp ! --syn -m state --state NEW -j DROP
+$IPTABLES -A COMMON -p tcp ! --syn -m state --state NEW -j DROP
 
 # Ouvertures des Ports Indispensable 
-sudo iptables -A TCP_IN -p tcp --dport 22 -j ACCEPT
-sudo iptables -A TCP_OUT -p tcp --destionation-port 80 -m limit --limit 3/s --limit-burst 6 -j ACCEPT
-sudo iptables -A TCP_OUT -p tcp --dport 22 -j ACCEPT
+$IPTABLES -A TCP_IN -p tcp --dport 22 -j ACCEPT
+$IPTABLES -A TCP_OUT -p tcp --destionation-port 80 -m limit --limit 3/s --limit-burst 6 -j ACCEPT
+$IPTABLES -A TCP_OUT -p tcp --dport 22 -j ACCEPT
 
 
 # Autoriser les connexions deja établies 
-sudo iptables -A COMMON -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES -A COMMON -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # Partie Indispensable
 # On autorise les connexions DNS sortantes
 for DNS in $DNS_SERVERS; do
-    sudo iptables -A UDP_OUT -p udp -d $DNS --sport 1024:65535 --dport 53 -j ACCEPT
+    $IPTABLES -A UDP_OUT -p udp -d $DNS --sport 1024:65535 --dport 53 -j ACCEPT
 done
 
 
 # Faire en soirte de que tout les autres paquets sortant en TCP soit Drop et Logger
-sudo iptables -A TCP_OUT -m limit --limit 2/min -j LOG --log-prefix "iptables TCP_OUT dropped: " --log-level 7
-sudo iptables -A TCP_OUT -j DROP
+$IPTABLES -A TCP_OUT -m limit --limit 2/min -j LOG --log-prefix "iptables TCP_OUT dropped: " --log-level 7
+$IPTABLES -A TCP_OUT -j DROP
 
 # Faire en soirte de que tout les autres paquets sortant en UDP soit Drop et Logger
-sudo iptables -A UDP_OUT -m limit --limit 2/min -j LOG --log-prefix "iptables UDP_OUT dropped: " --log-level 7
-sudo iptables -A UDP_OUT -j DROP
+$IPTABLES -A UDP_OUT -m limit --limit 2/min -j LOG --log-prefix "iptables UDP_OUT dropped: " --log-level 7
+$IPTABLES -A UDP_OUT -j DROP
 
 # Script pas encore terminé 
