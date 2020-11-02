46f25f3db6810a0f480f72d85e078b397ca8c04c
Johan_Paam <54694106+JohanPaam@users.noreply.github.com>
2020-11-02 09:31:49 +0100

    Update johan-script-v2.sh

--- johan-script-v2.sh
+++ johan-script-v2.sh
@@ -2,12 +2,21 @@
 
 ### Script By Johan_Paam ###
 # ATTENTION !!! #
+# V 1.2 Beta #
 # C'est un script de prévention contre les attaques, les attaques ne seront pas totalement contrer uniquement atténué et réduite au maximum ! #
+
+# Fermeture de tout les ports et ouverture des ports nécessaire
+iptables -A INPUT -p udp -m udp --dport 0:65565 -j DROP
+iptables -A INPUT -p tcp -m tcp --dport 0:65565 -j DROP
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 30120 -j ACCEPT
+iptables -A INPUT -p udp -m udp --dport 30120 -j ACCEPT
 
 # On autorise les connexions deja établies 
 iptables -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# On Drop tout les paquets malformer ou NULL 
+# On Drop tout les paquets malformer ou les paquets Nul
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 
 # On force la vérification des paquets de type SYN 
@@ -43,13 +52,10 @@ iptables -A INPUT -s 255.0.0.0/8 -j spoofing
 iptables -A INPUT -s 0.0.0.8/8 -j spoofing
 
 # Protection contre les attaques de type UDP 
-iptables -N UDP_IN
-iptables -A INPUT -p udp -j UDP_IN
-iptables -A UDP_IN -i enp1s0f0 -p udp --sport 30120 -m limit --limit 9/s --limit-burst 12 -j ACCEPT
-iptables -A UDP_IN -j DROP
+iptables -A INPUT -p udp --sport 30120 -m limit --limit 12/s --limit-burst 14 -j ACCEPT
 
 # Protection contre les attaques de type TCP SYN
-iptables -A INPUT -i eth0 -p tcp --syn --sport 30120 -m limit --limit 10/s -j ACCEPT
+iptables -A INPUT -p tcp  --syn --sport 30120 -m limit --limit 12/s -j ACCEPT
 
 # Protection contre les attaques de type SYN Flood par SYN Proxy
 iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
@@ -62,6 +68,9 @@ sudo sysctl -w net.ipv4.conf.all.rp_filter=1
 sudo sysctl -w net.ipv4.conf.all.log_martians=1
 sudo sysctl -w net.ipv4.conf.default.log_martians=1
 
-
-iptables -A INPUT -s 45.95.114.64 -j ACCEPT
-iptables -A OUTPUT -s 45.95.114.64 -j ACCEPT
+# Autorisation des différentes IPs de Five M
+iptables -A INPUT -s 104.22.46.177 -j ACCEPT # Liste Five M
+iptables -A INPUT -s 104.22.47.177 -j ACCEPT # Liste Five M
+iptables -A INPUT -s 172.67.38.114 -j ACCEPT # Servers Ingress Five M
+iptables -A INPUT -s 51.91.21.135 -j ACCEPT # Serveur d'authentification des clés Nucleus
+iptables -A INPUT -s 45.95.114.64 -j ACCEPT 
