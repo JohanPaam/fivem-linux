a7cdb9d751c8a8d0df328f87964571c2c436ee9d
Johan_Paam <54694106+JohanPaam@users.noreply.github.com>
2021-02-07 15:22:04 +0100

    Update auto-install-fivem.sh

--- auto-install-fivem.sh
+++ auto-install-fivem.sh
@@ -12,7 +12,6 @@ apt upgrade
 
 echo "Installation des paquets nécessaires"
 sleep 3
-tar xvfJ fx.tar.xz
 apt install git
 apt install sudo
 echo "Paquets installer avec succès"
@@ -23,11 +22,7 @@ sleep 3
 cd /home
 mkdir fivem
 cd /home/fivem
-git clone https://github.com/JohanPaam/cubix-fivem-install.git
-cd /home/fivem/cubix-install-fivem
-mv resources/ server.cfg /home/fivem
-cd /home/fivem
-rm -fr cubix-install-fivem/
+tar xvfJ fx.tar.xz
 cd
 echo "Serveur Five M installer dans le répertoire /home/fivem"
 sleep 6
@@ -40,20 +35,17 @@ echo "Installation de Maria-DB"
 sleep 3
 apt-get -y install mariadb-server mariadb-client
 mysql_secure_installation
-apt-get -y install php7.0 curl libapache2-mod-php7.0 build-essential apache2 php7.0-mysql aptgit php7.0-curl git php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php-apcu php7.0-ssh2 php7.0-opcache
 service apache2 restart
 
 echo "Installation de PhpMyAdmin"
 sleep 3
 apt install phpmyadmin
+sudo ln -s /usr/share/phpmyadmin /var/www/html
 
 echo "Création d'un utilisateur possédant les privièges Administrateur"
 mysql -u root -p
-CREATE USER 'auto-install'@'localhost' IDENTIFIED BY 'auto-install';
-GRANT ALL PRIVILEGES ON * . * TO 'auto-install'@'localhost';
+UPDATE mysql.user SET plugin = 'mysql_native_password',Password = PASSWORD('NEWPASSWORD') WHERE User = 'root';
 FLUSH PRIVILEGES;
-quit
-
 
 echo "Utilisateur : 'auto-install' avec comme mot de pass 'auto-install' a bien été crée"
 echo "Attenion ! Pensez a changer le mot de passe de cet utilisateur !"
